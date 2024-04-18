ed04b5d03260fb70e43a69ba7ebd085a4183976a
unknown <Ivancahwagu>
2024-04-18 08:17:18 +0700

    belajar

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['"./web/**/*.{html,js}"'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    upper:{
+      
+    },
+    extend: {
+      colors: {
+        'custom-blue': '#3366FF',
+        'hijau': '#14b8a6'
+      },
+      scale: {
+        '135': {
+          '--tw-scale-x': 1.35,
+          '--tw-scale-y': 1.35,
+          transform: 'translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));.'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
